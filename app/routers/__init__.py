16e8d81ff8d8296efc6cdaf209f23f2d44b41a0e
Chernique <hchernique@gmail.com>
2026-04-13 19:09:02 -0400

    Update __init__.py

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -9,7 +9,6 @@ static_files = StaticFiles(directory=os.path.join(os.path.dirname(__file__),".."
 templates = Jinja2Templates(directory=os.path.join(os.path.dirname(__file__),"..", "templates"))
 templates.env.globals['get_flashed_messages'] = get_flashed_messages
 
-
 from .auth import auth_router
 main_router.include_router(auth_router)
 
@@ -18,3 +17,9 @@ main_router.include_router(todo_router)
 
 from .home import home_router
 main_router.include_router(home_router)
+
+from .admin import admin_router
+main_router.include_router(admin_router)
+
+from .stats import stats_router
+main_router.include_router(stats_router)
